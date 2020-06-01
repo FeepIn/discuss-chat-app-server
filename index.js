a3f0534c54e84a9a0e4df2952c6123f96774b211
FeepIn <feepin.ytb@gmail.com>
2020-06-01 15:52:26 +0200

    port to PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 var namesTaken = [];
 
 var theme = {
