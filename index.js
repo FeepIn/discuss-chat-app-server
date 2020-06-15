774e979722295a1f2d3ede5a729b6e0986eff888
FeepIn <feepin.ytb@gmail.com>
2020-06-15 15:15:28 +0200

    minor changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 var namesTaken = [ "System" ];
 var colors = [ "#530008", "#00655E", "#471141", "#1567AB", "#4C46C7" ];
 var theme = {
@@ -184,7 +184,7 @@ function User(name, socket) {
 	configureListener();
 }
 
-server.listen(PORT, () => {
+server.listen(PORT, "192.168.1.39", () => {
 	console.log(`Server is listening on port ${PORT}...`);
 });
 
